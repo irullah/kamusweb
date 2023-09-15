94877354c75458874b41c8b0c9a6651954f02492
irullah <okeoke018@gmail.com>
2023-09-15 09:51:24 +0700

    tambah petunjuk

--- index.php
+++ index.php
@@ -31,14 +31,29 @@
 </nav>
 
 <div style="background-color: #0099ff;">
-<div class="container text-center">
-  <div style="padding-top: 30px;" class="row">
-    <div class="col-2"></div>
-    <div class="col-8">
+<div class="container">
+  <div style="padding-top: 60px;" class="row">
+    <div class="col-3">
+        <div class="alert text-left alert-warning" role="alert">
+            <h4 class="alert-heading">Petunjuk</h4>
+            <p class="mb-0">Menuliskan <strong>^a</strong> berubah menjadi <strong>â</strong></p>
+            <p class="mb-0">Menuliskan <strong>`e</strong> berubah menjadi <strong>è</strong><br><br>Contoh :</p>
+            <ul>
+                <li>
+                    <strong>l`eker</strong> &rarr; <strong>lèker</strong>
+                </li>
+                <li>
+                    <strong>ad^a'</strong>  &rarr; <strong>adâ'</strong>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="col-1"></div>
+    <div class="col-6">
         <form action="" method="get">
-            <h1 >Kamus Bahasa Madura</h1>
+            <h1 class="text-center" >Kamus Bahasa Madura</h1>
             <div class="input-group mb-3">
-                <input name="kata" type="cari" class="form-control" placeholder="Kata" aria-label="Kata" aria-describedby="button-addon2">
+                <input name="kata" type="cari" class="form-control" placeholder="Kata" aria-label="Kata" aria-describedby="button-addon2" id="kunci" onchange="myFunction(this.value)">
                 <button class="btn btn-primary" type="submit" id="button-addon2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
@@ -48,7 +63,6 @@
             </div>
         </form>
         </div>
-        <div class="col-2"></div>
     </div>
     </div>
 </div>
@@ -90,5 +104,36 @@
 
             
 </body>
+<script>
+function myFunction(val) {
+    val = val.toLowerCase()
+    let hasil = "";
+
+    for (let i = 0; i < val.length; i++) 
+    {
+        if (val.substring(i, i+2) === "^a") 
+        {
+            hasil += "â";
+        }
+        else if (val.substring(i-1, i+1) === "^a")
+        {
+            hasil += "";
+        }
+        else if (val.substring(i, i+2) === "`e") 
+        {
+            hasil += "è";
+        }
+        else if (val.substring(i-1, i+1) === "`e")
+        {
+            hasil += "";
+        }
+        else 
+        {
+            hasil += val.substring(i, i+1);
+        }
+    }
+    document.getElementById("kunci").value = hasil;
+}
+</script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </html>
