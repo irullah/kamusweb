3b692d76f1497eafe543330d98d948dc17f3f6cc
irullah <okeoke018@gmail.com>
2023-12-15 13:28:14 +0700

    baru

---
+++ about.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+<div style="background-color: #0099ff;">
+<div class="container" id="background-biru">
+    <div style="padding-top: 60px;" class="row">
+        <h3 class="judul"><b>TENTANG APLIKASI</b></h3>
+        <div class="col-5">
+            <img style="margin-top:100px; height:500px; width:500px;" src = "assets\gambar\programming_and_web_technology_vector.png" alt="Wave"/>
+        </div>
+        <div class="col-1"></div>
+        <div class="col-6" style="margin-top:10%;" id="sekilas">
+            <h4><b>Tentang Aplikasi Kamus Bahasa Madura-Indonesia Daring</b></h4>
+            <p>Aplikasi Kamus Bahasa Madura-Indonesia ini merupakan kamus daring yang basisdatanya mengacu pada MadureseSet [1], yaitu hasil digitalisasi dari Kamus Lengkap Bahasa Madura Indonesia [2] versi cetak. Aplikasi ini dibuat untuk memudahkan pencarian dan contoh penggunaan suatu kata (lema/sub lema) dalam Bahasa Madura sekaligus artinya dalam Bahasa Indonesia. Dengan demikian, aplikasi ini dapat menjadi alat penting bagi pembelajar bahasa, peneliti, penulis, dan siapa pun yang membutuhkan bantuan cepat dan akurat dalam memahami kosakata Bahasa Madura.</p>
+            <br>
+            <h4><b>Fitur Aplikasi Kamus Bahasa Madura-Indonesia Daring</b></h4>
+            <p>Aplikasi ini memiliki daftar fitur berikut:</p>
+            <ul>
+                <li>Cara menuliskan karakter khusus/beraksen (â, è, ḍ, dan ṭ) yang ada dalam Bahasa Madura</li>
+                <li>Pencarian satu kata (aplikasi akan memberikan daftar saran kata yang mungkin dimaksud jika kata yang dicari tidak ada di dalam kamus [3])</li>
+                <li>Arti kata (dapat lebih dari satu jika merupakan kata homonim)</li>
+                <li>Contoh penggunaan kata</li>
+                <li>Keterangan kata (lema/sub lema): kelas kata, dialek, tingkatan bahasa, serapan bahasa asing, serapan bahasa daerah dan lainnya</li>
+                <li>Pranala antar kata (lema/sub lema)</li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6" style="margin-top:5%;" id="sekilas">
+            <h4><b>Referensi</b></h4>
+            <p>Aplikasi ini mengacu pada 2 jurnal, yaitu:</p>
+            <ol>
+                <li>N. Ifada, F. H. Rachman, M. W. M. A. Syauqy, S. Wahyuni, and A. Pawitra, "MadureseSet: Madurese-Indonesian Dataset." <i>Data in Brief 48</i> (2023): 109035. DOI: <a href="https://doi.org/10.1016/j.dib.2023.109035">https://doi.org/10.1016/j.dib.2023.109035</a></li>
+                <li><a href="http://refhub.elsevier.com/S2352-3409(23)00153-1/sbref0001">A. Pawitra, Kamus Lengkap Bahasa Madura-Indonesia [The Complete Dictionary of Madurese-Indonesian], Dian Rakyat, Jakarta, 2009.</a></li>
+                <li>N. Ifada, F. H. Rachman, and S. Wahyuni, "Character-based String Matching Similarity Algorithms for Madurese Spelling Correction: A Preliminary Study," in <i>International Conference on Electrical Engineering and Informatics (ICEEI) 2023</i>, Bandung, 2023, p. [In Press]: IEEE.</li>
+            </ol>
+        </div>
+        <div class="col-1"></div>
+        <div class="col-5">
+            <img style="height:400px; width:400px;" src = "assets\gambar\books.png" alt="Books"/>
+        </div>
+    </div>
+</div>
+</div>
+<img src = "assets/gambar/wave.svg" alt="Wave"/>
+
+<h3 class="judul"><b>TIM PENGEMBANG APLIKASI</b></h3>
+<section class="tim">
+    <div class="square">
+        <div class="member-block">
+            <div class="member-block-image-wrap">
+                <img src="assets/tim/ifada.jpg" class="member-block-image img-fluid" alt="">
+
+                <ul class="social-icon">
+                    <li class="social-icon-item">
+                        <a href="mailto:noor.ifada@trunojoyo.ac.id" class="social-icon-link bi bi-envelope-fill"></a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="member-block-info d-flex align-items-center">
+                <h4>Noor Ifada</h4>
+            </div>
+        </div>
+    </div>
+    <div class="square">
+        <div class="member-block">
+            <div class="member-block-image-wrap">
+                <img src="" class="member-block-image img-fluid" alt="Ibu Fika">
+
+                <ul class="social-icon">
+                    <li class="social-icon-item">
+                        <a href="mailto:" class="social-icon-link bi bi-envelope-fill"></a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="member-block-info d-flex align-items-center">
+                <h4>Fika Hastarita Rachman</h4>
+            </div>
+        </div>
+    </div>
+    <div class="square">
+       
+        <div class="member-block">
+            <div class="member-block-image-wrap">
+                <img src="assets/tim/wildan.jpeg" class="member-block-image img-fluid" alt="">
+
+                <ul class="social-icon">
+                    <li class="social-icon-item">
+                        <a href="mailto:willnode@wellosoft.net" class="social-icon-link bi bi-envelope-fill"></a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="member-block-info d-flex align-items-center">
+                <h4> M Wildan Mubarok Asy Syauqy</h4>
+            </div>
+        </div>
+    </div>
+    <div class="square">
+        
+        <div class="member-block">
+            <div class="member-block-image-wrap">
+                <img src="assets/tim/wahyuni.jpeg" class="member-block-image img-fluid" alt="">
+
+                <ul class="social-icon">
+                    <li class="social-icon-item">
+                        <a href="mailto:s.wahyuni@trunojoyo.ac.id" class="social-icon-link bi bi-envelope-fill"></a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="member-block-info d-flex align-items-center">
+                <h4>Sri Wahyuni</h4>
+            </div>
+        </div>
+    </div>
+    <div class="square">
+        
+        <div class="member-block">
+            <div class="member-block-image-wrap">
+                <img src="assets/tim/andrian.jpeg" class="member-block-image img-fluid" alt="">
+
+            </div>
+
+            <div class="member-block-info d-flex align-items-center">
+                <h4>Adrian Pawitra</h4>
+            </div>
+        </div>
+    </div>
+    <div class="square">
+        <div class="member-block">
+            <div class="member-block-image-wrap">
+                <img src="assets/tim/irul.jpg" class="member-block-image img-fluid" alt="">
+
+                <ul class="social-icon">
+                    <li class="social-icon-item">
+                        <a href="mailto:moh.amirullah17@gmail.com" class="social-icon-link bi bi-envelope-fill"></a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="member-block-info d-flex align-items-center">
+                <h4>Moh Amirullah</h4>
+            </div>
+        </div>
+    </div>
+    <div class="square">
+        <div class="member-block">
+            <div class="member-block-image-wrap">
+                <img src="assets/tim/thoriq.jpeg" class="member-block-image img-fluid" alt="">
+
+                <ul class="social-icon">
+                    <li class="social-icon-item">
+                        <a href="mailto:thoriq771@gmail.com" class="social-icon-link bi bi-envelope-fill"></a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="member-block-info d-flex align-items-center">
+                <h4>Muhammad Fathuthoriq</h4>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- start footer -->
+<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-muted">&copy; 2024 Universitas Trunojoyo Madura, Inc</p>
+
+    <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><a href="http://localhost/kamus/" class="nav-link px-2 text-muted">Kamus</a></li>
+      <li class="nav-item"><a href="http://localhost/kamus/about.php" class="nav-link px-2 text-muted">About</a></li>
+    </ul>
+  </footer>
+</div>
+<!-- end footer -->
+
+</body>
+<script src="assets/bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+<script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+</html>
--- index.php
+++ index.php
@@ -3,46 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Translate</title>
+    <title>Kamus</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/bootstrap/icons/bootstrap-icons.css">
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
 <!-- start navbar -->
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<!-- <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Kamus</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
         <ul class="nav justify-content-end nav-underline">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Active</a>
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
             </li>
         </ul>
   </div>
-</nav>
+</nav> -->
 <!-- end navbar -->
 
 <div style="background-color: #0099ff;">
 <div class="container">
   <div style="padding-top: 60px;" class="row">
     <div class="col-3">
-        <div class="alert text-left alert-warning" role="alert">
+        <div class="alert text-left alert-warning" style="font-family: 'Courier, monospace;" role="alert">
             <h4 class="alert-heading">Petunjuk</h4>
+            <p class="mb-0">Menuliskan <strong>.d</strong> berubah menjadi <strong>ḍ</strong></p>
+            <p class="mb-0">Menuliskan <strong>.t</strong> berubah menjadi <strong>ṭ</strong></p>
             <p class="mb-0">Menuliskan <strong>^a</strong> berubah menjadi <strong>â</strong></p>
             <p class="mb-0">Menuliskan <strong>`e</strong> berubah menjadi <strong>è</strong><br><br>Contoh :</p>
             <ul>
+                <li>
+                    <strong>ro.du</strong> &rarr; <strong>roḍu</strong>
+                </li>
+                <li>
+                    <strong>sal`e.t`eng</strong> &rarr; <strong>salèṭèng</strong>
+                </li>
                 <li>
                     <strong>l`eker</strong> &rarr; <strong>lèker</strong>
                 </li>
@@ -66,7 +68,7 @@
     </div>
     </div>
 </div>
-<img src = "assets/waves/wave.svg" alt="Wave"/>
+<img src = "assets/gambar/wave.svg" alt="Wave"/>
 <div class="row">
     <div class="col-1"></div>
     <div class="col-10">
@@ -111,11 +113,63 @@
                 </div>
             </div> 
             <!-- end arti -->
-            
+
             <br>
 
+            <!-- start kalimat -->
+            <div class="card">
+                <div class="card-header">
+                    <h4><strong>Contoh penggunaan kata</strong></h4>
+                </div>
+                <div class="card-body">
+                <?php
+        $index_kalimat = 1;
+        $kalimat =  mysqli_query($koneksi, "SELECT lemmata.id, lemmata.basic_lemma, lemmata.pronunciation, sentences.sentence, substitution_lemmata.POS, part_of_speech.description FROM lemmata RIGHT JOIN sentences on lemmata.id = sentences.lemma_id LEFT JOIN substitution_lemmata on sentences.id = substitution_lemmata.sentence_id LEFT JOIN descr_subs_lemmata on substitution_lemmata.description = descr_subs_lemmata.key LEFT JOIN part_of_speech on substitution_lemmata.POS = part_of_speech.tag WHERE language = 'MAD' and basic_lemma LIKE ".$kata." GROUP BY lemmata.id ;");
+        while ($data_kalimat = mysqli_fetch_array($kalimat)) { 
+                    $pos = $data_kalimat['POS'];
+                    $ket = cekKetPos($pos.".");
+                    ?>
+                    <h6 class="card-title" id="kalimat-<?= $index_kalimat ?>"> <b><?= $data_kalimat["basic_lemma"] ?></b><sub onclick="scrollToContent()" id="pointer">[<?= $index_kalimat ?>]</sub> 
+                    <span id="tooltip">
+                        <span id="tooltiptext">Kelas kata : <?= $ket." (<i>".$data_kalimat['description']."</i>)" ?></span>
+                        <span><i><?= $pos ?></i></span>
+                    </span>
+                    </h6>
+                    <ul>
+                    <?php
+
+        $kumpulan_kalimat =  mysqli_query($koneksi, "SELECT * FROM `sentences`JOIN lemmata ON lemmata.id = sentences.lemma_id WHERE sentences.language = 'MAD' AND lemmata.homonym_index = ".$index_kalimat." AND lemmata.basic_lemma = ".$kata.";");
+        $baris = 1;
+        
+        while ($data_kumpulan_kalimat = mysqli_fetch_array($kumpulan_kalimat)) { 
+            if ($kumpulan_kalimat->num_rows >= 2) {
+                if ($baris >= 2){
+                    $string = $data_kumpulan_kalimat['sentence'];
+                    $string = Normalisasi($string);
+                    $string = addLink($string, $_GET['kata'], $koneksi);
+                    $string = cekPos($string, $koneksi);
+                    $string = cekJenis($string, $koneksi);
+                    $string = cekKategori($string, $koneksi);
+                    $sentencesIND = mysqli_query($koneksi, "SELECT * FROM sentences WHERE sentences.language = 'IND' AND sentences.lemma_id = ".$data_kumpulan_kalimat['lemma_id']." AND sentences.index = '".$data_kumpulan_kalimat['index']."';");
+                    $data_kalimat_ind = mysqli_fetch_array($sentencesIND);?>
+                                <li><p class="card-text"><?= $string ?> <br> &rarr; <span id="lang-id"><?= $data_kalimat_ind['sentence'] ?></span></p></li>
+                <?php } $baris++; }
+            else {
+                echo '<script>document.getElementById("kalimat-'.$index_kalimat.'").innerHTML = "";</script>';
+            }
+            }
+            ?>
+            </ul>
+            <?php
+        
+    $index_kalimat++; } 
+    ?>
+                </div>
+            </div> 
+            <!-- end kalimat -->
+            <br>
             <!-- start pengucapan -->
-            <div class="card" >
+            <div class="card" id="card-pengucapan">
                 <div class="card-header">
                     <h4><strong>Pengucapan</strong></h4>
                 </div>
@@ -148,66 +202,14 @@
                 </div> 
                 <!-- end pengucapan -->
 
-                <br>
-
-                <!-- start kalimat -->
-                <div class="card">
-                    <div class="card-header">
-                        <h4><strong>Contoh penggunaan kata</strong></h4>
-                    </div>
-                    <div class="card-body">
-                    <?php
-            $index_kalimat = 1;
-            $kalimat =  mysqli_query($koneksi, "SELECT lemmata.id, lemmata.basic_lemma, lemmata.pronunciation, sentences.sentence, substitution_lemmata.POS, part_of_speech.description FROM lemmata RIGHT JOIN sentences on lemmata.id = sentences.lemma_id LEFT JOIN substitution_lemmata on sentences.id = substitution_lemmata.sentence_id LEFT JOIN descr_subs_lemmata on substitution_lemmata.description = descr_subs_lemmata.key LEFT JOIN part_of_speech on substitution_lemmata.POS = part_of_speech.tag WHERE language = 'MAD' and basic_lemma LIKE ".$kata." GROUP BY lemmata.id ;");
-            while ($data_kalimat = mysqli_fetch_array($kalimat)) { 
-                        $pos = $data_kalimat['POS'];
-                        $ket = cekKetPos($pos.".");
-                        ?>
-                        <h6 class="card-title" id="kalimat-<?= $index_kalimat ?>"> <b><?= $data_kalimat["basic_lemma"] ?></b><sub onclick="scrollToContent()" id="pointer">[<?= $index_kalimat ?>]</sub> 
-                        <span id="tooltip">
-                            <span id="tooltiptext">Kelas kata : <?= $ket." (<i>".$data_kalimat['description']."</i>)" ?></span>
-                            <span><i><?= $pos ?></i></span>
-                        </span>
-                        </h6>
-                        <ul>
-                        <?php
-
-            $kumpulan_kalimat =  mysqli_query($koneksi, "SELECT * FROM `sentences`JOIN lemmata ON lemmata.id = sentences.lemma_id WHERE sentences.language = 'MAD' AND lemmata.homonym_index = ".$index_kalimat." AND lemmata.basic_lemma = ".$kata.";");
-            $baris = 1;
-            
-            while ($data_kumpulan_kalimat = mysqli_fetch_array($kumpulan_kalimat)) { 
-                if ($kumpulan_kalimat->num_rows >= 2) {
-                    if ($baris >= 2){
-                        $string = $data_kumpulan_kalimat['sentence'];
-                        $string = Normalisasi($string);
-                        $string = cekPos($string, $koneksi);
-                        $string = cekJenis($string, $koneksi);
-                        $string = cekKategori($string, $koneksi);
-                        $sentencesIND = mysqli_query($koneksi, "SELECT * FROM sentences WHERE sentences.language = 'IND' AND sentences.lemma_id = ".$data_kumpulan_kalimat['lemma_id']." AND sentences.index = '".$data_kumpulan_kalimat['index']."';");
-                        $data_kalimat_ind = mysqli_fetch_array($sentencesIND);?>
-                                    <li><p class="card-text"><?= $string ?> <br> &rarr; <span id="lang-id"><?= $data_kalimat_ind['sentence'] ?></span></p></li>
-                    <?php } $baris++; }
-                else {
-                    echo '<script>document.getElementById("kalimat-'.$index_kalimat.'").innerHTML = "";</script>';
-                }
-                }
-                ?>
-                </ul>
-                <?php
-            
-        $index_kalimat++; } 
-        ?>
-                    </div>
-                </div> 
-                <!-- end kalimat -->
+                
                 <br> 
         <?php
         } else { 
         ?> 
             <h4>Tidak ada kata <strong>"<?= $_GET['kata'] ?>"</strong>  dalam bahasa madura. Mungkin maksud Anda: </h4>
-            
         <?php
-            
+
 
             $kata = $_GET['kata'];
             $lemmata = mysqli_query($koneksi, "SELECT basic_lemma FROM lemmata");
@@ -232,14 +234,11 @@
 <!-- start footer -->
 <div class="container">
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <p class="col-md-4 mb-0 text-muted">&copy; 2022 Company, Inc</p>
+    <p class="col-md-4 mb-0 text-muted">&copy; 2024 Universitas Trunojoyo Madura, Inc</p>
 
     <ul class="nav col-md-4 justify-content-end">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+      <li class="nav-item"><a href="http://localhost/kamus/" class="nav-link px-2 text-muted">Kamus</a></li>
+      <li class="nav-item"><a href="http://localhost/kamus/about.php" class="nav-link px-2 text-muted">About</a></li>
     </ul>
   </footer>
 </div>
@@ -269,6 +268,22 @@ function myFunction(val) {
             hasil += "è";
         }
         else if (val.substring(i-1, i+1) === "`e")
+        {
+            hasil += "";
+        }
+        else if (val.substring(i, i+2) === ".d") 
+        {
+            hasil += "ḍ";
+        }
+        else if (val.substring(i-1, i+1) === ".d")
+        {
+            hasil += "";
+        }
+        else if (val.substring(i, i+2) === ".t") 
+        {
+            hasil += "ṭ";
+        }
+        else if (val.substring(i-1, i+1) === ".t")
         {
             hasil += "";
         }
